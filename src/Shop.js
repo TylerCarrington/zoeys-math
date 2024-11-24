21d3b1b42e81e1f2893843c93ade4f6f1adfd0af
tylercarrington <TC_Carrington@yahoo.com>
2024-11-24 20:49:05 +0000

    Fix the images

--- src/Shop.js
+++ src/Shop.js
@@ -6,63 +6,63 @@ const Shop = ({ coins, tickets, setCoins, setTickets, setPage }) => {
 
   // Animal Pack and Baseball Pack Images
   const animalImages = [
-    "/images/animals/animal1.webp",
-    "/images/animals/animal2.webp",
-    "/images/animals/animal3.webp",
-    "/images/animals/animal4.webp",
-    "/images/animals/animal5.webp",
-    "/images/animals/animal6.webp",
-    "/images/animals/animal7.webp",
-    "/images/animals/animal8.webp",
-    "/images/animals/animal9.webp",
-    "/images/animals/animal10.webp",
-    "/images/animals/animal11.webp",
-    "/images/animals/animal12.webp",
-    "/images/animals/animal13.webp",
-    "/images/animals/animal14.webp",
-    "/images/animals/animal15.webp",
-    "/images/animals/animal16.webp",
-    "/images/animals/animal17.webp",
-    "/images/animals/animal18.webp",
-    "/images/animals/animal19.webp",
-    "/images/animals/animal20.webp",
-    "/images/animals/animal21.webp",
-    "/images/animals/animal22.webp",
-    "/images/animals/animal23.webp",
-    "/images/animals/animal24.webp",
-    "/images/animals/animal25.webp",
-    "/images/animals/husky1.png",
-    "/images/animals/husky2.png",
-    "/images/animals/husky3.png",
+    "./images/animals/animal1.webp",
+    "./images/animals/animal2.webp",
+    "./images/animals/animal3.webp",
+    "./images/animals/animal4.webp",
+    "./images/animals/animal5.webp",
+    "./images/animals/animal6.webp",
+    "./images/animals/animal7.webp",
+    "./images/animals/animal8.webp",
+    "./images/animals/animal9.webp",
+    "./images/animals/animal10.webp",
+    "./images/animals/animal11.webp",
+    "./images/animals/animal12.webp",
+    "./images/animals/animal13.webp",
+    "./images/animals/animal14.webp",
+    "./images/animals/animal15.webp",
+    "./images/animals/animal16.webp",
+    "./images/animals/animal17.webp",
+    "./images/animals/animal18.webp",
+    "./images/animals/animal19.webp",
+    "./images/animals/animal20.webp",
+    "./images/animals/animal21.webp",
+    "./images/animals/animal22.webp",
+    "./images/animals/animal23.webp",
+    "./images/animals/animal24.webp",
+    "./images/animals/animal25.webp",
+    "./images/animals/husky1.png",
+    "./images/animals/husky2.png",
+    "./images/animals/husky3.png",
   ];
   const baseballImages = [
-    "/images/baseball/baseball1.jpeg",
-    "/images/baseball/baseball2.jpeg",
-    "/images/baseball/baseball3.jpeg",
-    "/images/baseball/baseball4.jpeg",
-    "/images/baseball/baseball5.jpeg",
-    "/images/baseball/baseball6.jpeg",
-    "/images/baseball/baseball7.jpeg",
-    "/images/baseball/baseball8.jpeg",
-    "/images/baseball/baseball9.jpeg",
-    "/images/baseball/baseball10.jpeg",
-    "/images/baseball/baseball11.jpeg",
-    "/images/baseball/baseball12.jpeg",
-    "/images/baseball/baseball13.jpeg",
-    "/images/baseball/baseball14.jpeg",
-    "/images/baseball/baseball15.jpeg",
-    "/images/baseball/baseball16.jpeg",
-    "/images/baseball/baseball17.jpeg",
-    "/images/baseball/baseball18.jpeg",
-    "/images/baseball/baseball19.jpeg",
-    "/images/baseball/baseball20.jpeg",
-    "/images/baseball/baseball21.jpeg",
-    "/images/baseball/baseball22.jpeg",
-    "/images/baseball/baseball23.jpeg",
-    "/images/baseball/baseball24.jpeg",
-    "/images/baseball/baseball25.jpeg",
-    "/images/baseball/baseball26.webp",
-    "/images/baseball/baseball27.jpeg",
+    "./images/baseball/baseball1.jpeg",
+    "./images/baseball/baseball2.jpeg",
+    "./images/baseball/baseball3.jpeg",
+    "./images/baseball/baseball4.jpeg",
+    "./images/baseball/baseball5.jpeg",
+    "./images/baseball/baseball6.jpeg",
+    "./images/baseball/baseball7.jpeg",
+    "./images/baseball/baseball8.jpeg",
+    "./images/baseball/baseball9.jpeg",
+    "./images/baseball/baseball10.jpeg",
+    "./images/baseball/baseball11.jpeg",
+    "./images/baseball/baseball12.jpeg",
+    "./images/baseball/baseball13.jpeg",
+    "./images/baseball/baseball14.jpeg",
+    "./images/baseball/baseball15.jpeg",
+    "./images/baseball/baseball16.jpeg",
+    "./images/baseball/baseball17.jpeg",
+    "./images/baseball/baseball18.jpeg",
+    "./images/baseball/baseball19.jpeg",
+    "./images/baseball/baseball20.jpeg",
+    "./images/baseball/baseball21.jpeg",
+    "./images/baseball/baseball22.jpeg",
+    "./images/baseball/baseball23.jpeg",
+    "./images/baseball/baseball24.jpeg",
+    "./images/baseball/baseball25.jpeg",
+    "./images/baseball/baseball26.webp",
+    "./images/baseball/baseball27.jpeg",
   ];
 
   // Owned items (fetched from local storage)
@@ -147,7 +147,7 @@ const Shop = ({ coins, tickets, setCoins, setTickets, setPage }) => {
         <div>
           <h2>Animal Pack</h2>
           <img
-            src={"/images/packs/animal-pack.png"}
+            src={"./images/packs/animal-pack.png"}
             alt="Owned Item"
             style={{ width: "100px", height: "100px", objectFit: "contain" }}
           />
@@ -168,7 +168,7 @@ const Shop = ({ coins, tickets, setCoins, setTickets, setPage }) => {
         <div>
           <h2>Baseball Pack</h2>
           <img
-            src={"/images/packs/baseball-pack.png"}
+            src={"./images/packs/baseball-pack.png"}
             alt="Owned Item"
             style={{ width: "100px", height: "100px", objectFit: "contain" }}
           />
